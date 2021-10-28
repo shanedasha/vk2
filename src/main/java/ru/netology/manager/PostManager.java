8ac04325cc8391df698c4047c7894f97195ba03a
shanedasha <chirkova_99@inbox.ru>
2021-10-28 16:28:22 +0300

    Third commit

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,14 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] Posts;
+
+    public Post[] search(String ownerId, String domain, String query, String ownersOnly, String count, String offset, String extended, String fields) {
+        return null;
+    }
+
+    public void delete(String ownerId, String postId) {
+    }
+}
